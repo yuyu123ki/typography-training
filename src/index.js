8739dafcb21def090059e7c18fba812f3ede5bb5
hrdtbs <harada.tsubasa.0422@gmail.com>
2019-08-29 00:10:07 +0900

    feat(*): update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+import story from "./alice.md";
+
+document.getElementById("story").insertAdjacentHTML(
+  "afterBegin",
+  story
+    .split(/(?=<h3[^>]*>)/g)
+    .map((f, i) => (i ? `<section class="container">${f}</section>` : f))
+    .join("")
+);
